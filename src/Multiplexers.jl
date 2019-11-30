d4c76100348acc11bc822388b8c0376851b2586d
akels <akels14@gmail.com>
2019-11-30 01:25:58 +0100

    kill tasks

--- src/Multiplexers.jl
+++ src/Multiplexers.jl
@@ -20,7 +20,7 @@ deserialize(line::Line) = take!(line.ch)
 """
 Takes multiple input lines and routes them to a single line.
 """
-function route(lines::Vector{Line},socket)
+function route(lines::Vector{Line},socket::IO)
     while true
         data = deserialize(socket)
         if data==:Terminate
@@ -36,23 +36,30 @@ end
 """
 A function which one uses to forward forward traffic from multiple sockets into one socket by multiplexing.
 """
-function route(ios::Vector{IO},socket)
+function route(ios::Vector{IO},socket::IO)
     lines = [Line(socket,i) for i in 1:length(ios)]
     task = @async route(lines,socket)
     
+    tasks = []
+
     for (line,io) in zip(lines,ios)
-        @async while true
+        task1 = @async while true
             msg = deserialize(line)
             serialize(io,msg)
-        end
+        end 
+        push!(tasks,task1)
         
-        @async while true
+        task2 = @async while true
             msg = deserialize(io)
             serialize(line,msg)
         end
+        push!(tasks,task2)
     end
 
     wait(task)
+    for t in tasks
+        @async Base.throwto(t,InterruptException()) 
+    end
 end
 
 export Line, route, serialize, deserialize
